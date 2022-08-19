abe011957488d910d092b7c23244e991eb7862dd
kosovic-drasko <kosovic.drasko@montefarm.co.me>
2022-08-19 20:08:38 +0200

    novi 2

--- src/main/java/tender/repository/PonudeRepository.java
+++ src/main/java/tender/repository/PonudeRepository.java
@@ -1,6 +1,9 @@
 package tender.repository;
 
+import java.util.List;
+import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import tender.domain.Ponude;
 
@@ -9,4 +12,22 @@ import tender.domain.Ponude;
  */
 @SuppressWarnings("unused")
 @Repository
-public interface PonudeRepository extends JpaRepository<Ponude, Long>, JpaSpecificationExecutor<Ponude> {}
+public interface PonudeRepository extends JpaRepository<Ponude, Long>, JpaSpecificationExecutor<Ponude> {
+    @Modifying
+    @Transactional
+    @Query("delete from Ponude p where p.sifraPonude=:sifraPonude")
+    void deletePonudeSifraPonude(@Param("sifraPonude") Integer sifraPonude);
+
+    @Query("select p from Ponude p where p.sifraPostupka=:sifraPostupka")
+    List<Ponude> findBySifraPostupkaList(@Param("sifraPostupka") Integer sifraPostupka);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE from Ponude p WHERE p.selected = true")
+    void deleteBySelected();
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Ponude p SET p.selected=true WHERE p.id = :Id")
+    void updateSlected(@Param("Id") Long Id);
+}
