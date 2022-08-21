bfa9610da9004b79c95306aa694556c5e21559f0
kosovic-drasko <kosovic.drasko@montefarm.co.me>
2022-08-21 09:55:51 +0200

    nedelja 21.08 br3

--- src/main/webapp/app/entities/entity-routing.module.ts
+++ src/main/webapp/app/entities/entity-routing.module.ts
@@ -39,6 +39,11 @@ import { RouterModule } from '@angular/router';
         data: { pageTitle: 'tenderApp.prvorangirani.home.title' },
         loadChildren: () => import('./prvorangirani/prvorangirani.module').then(m => m.PrvorangiraniModule),
       },
+      {
+        path: 'hvale',
+        data: { pageTitle: 'tenderApp.prvorangirani.home.title' },
+        loadChildren: () => import('./hvale-ponude/hvale-ponude.module').then(m => m.HvalePonudeModule),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
