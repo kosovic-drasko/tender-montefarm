6af3856c9c6bfc9715e42c0de1ce1b3b5ad8bad3
kosovic-drasko <kosovic.drasko@montefarm.co.me>
2022-09-03 23:42:53 +0200

    material table

--- src/main/webapp/app/entities/vrednovanje/vrednovanje.module.ts
+++ src/main/webapp/app/entities/vrednovanje/vrednovanje.module.ts
@@ -3,9 +3,11 @@ import { SharedModule } from 'app/shared/shared.module';
 import { VrednovanjeComponent } from './list/vrednovanje.component';
 import { VrednovanjeDetailComponent } from './detail/vrednovanje-detail.component';
 import { VrednovanjeRoutingModule } from './route/vrednovanje-routing.module';
+import { MatTableExporterModule } from 'mat-table-exporter';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
-  imports: [SharedModule, VrednovanjeRoutingModule],
+  imports: [SharedModule, VrednovanjeRoutingModule, MatTableExporterModule, MatSortModule],
   declarations: [VrednovanjeComponent, VrednovanjeDetailComponent],
   exports: [VrednovanjeComponent],
 })
